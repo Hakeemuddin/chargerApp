5144ec14f886eae24768a80cbd04801724fc7233
Hakeem <hakeem@alpha2k.io>
2021-03-05 18:20:34 -0500

    removed firebase config object

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDoEcBpfpODPjzWoYLGBtKi0BjHwHGfl5M",
-    authDomain: "charger-app-b674f.firebaseapp.com",
-    projectId: "charger-app-b674f",
-    storageBucket: "charger-app-b674f.appspot.com",
-    messagingSenderId: "270870024102",
-    appId: "1:270870024102:web:93ee31bfaf46508903c967",
-    measurementId: "G-XSKE1W6Y13"
-  }
+  // firebase: {
+  //   apiKey: "apiKey",
+  //   authDomain: "authDomain",
+  //   projectId: "projectId",
+  //   storageBucket: "storageBucket",
+  //   messagingSenderId: "messagingSenderId",
+  //   appId: "appId",
+  //   measurementId: "measurementId"
+  // }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDoEcBpfpODPjzWoYLGBtKi0BjHwHGfl5M",
-    authDomain: "charger-app-b674f.firebaseapp.com",
-    projectId: "charger-app-b674f",
-    storageBucket: "charger-app-b674f.appspot.com",
-    messagingSenderId: "270870024102",
-    appId: "1:270870024102:web:93ee31bfaf46508903c967",
-    measurementId: "G-XSKE1W6Y13"
-  }
+  // firebase: {
+  //   apiKey: "apiKey",
+  //   authDomain: "authDomain",
+  //   projectId: "projectId",
+  //   storageBucket: "storageBucket",
+  //   messagingSenderId: "messagingSenderId",
+  //   appId: "appId",
+  //   measurementId: "measurementId"
+  // }
 };
 
 /*
